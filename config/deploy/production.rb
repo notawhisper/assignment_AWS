e70531edefb8b3979465a697b78d58342b28b29a
shotaro yamamoto <shotoron@gmail.com>
2020-09-18 16:04:02 +0900

    production修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,7 @@
-server '54.238.31.113', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/shotoron/.ssh/id_rsa'
+server '54.238.31.113',
+  user: 'app',
+  roles: %w{app db web},
+  ssh_options: {
+    user: "app",
+    keys: '/Users/shotoron/.ssh/id_rsa'
+  }
